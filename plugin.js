77bc41013e2e86ec11ad5d89c4cab8374d6b2ef5
schmyga <93492278+schmyga@users.noreply.github.com>
2025-10-06 20:33:40 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,28 +1,15 @@
-// Androzon Plugin for Lampa
-var plugin = {};
-
-plugin.name = 'Androzon';
-plugin.version = '1.0.0';
-plugin.description = 'Search plugin';
-
-plugin.search = function(text, type) {
-    return new Promise((resolve) => {
-        console.log('Androzon search:', text);
-        
-        var results = [{
-            title: 'Androzon: ' + text,
-            description: 'Плагин работает! 🎉',
-            year: '2024',
-            type: type || 'movie',
-            icon: 'https://raw.githubusercontent.com/lampa-player/lampa/master/src/img/icon.png'
-        }];
-        
-        resolve(results);
-    });
+// Androzon
+this["Androzon"] = {
+    name: "Androzon",
+    version: "1.0.0",
+    
+    search: function(text) {
+        return new Promise(function(resolve){
+            resolve([{
+                title: "Androzon: " + text,
+                description: "РАБОТАЕТ!",
+                type: "movie"
+            }]);
+        });
+    }
 };
-
-// Регистрация в Lampa
-if(typeof Manager !== 'undefined') {
-    Manager.add(plugin);
-    console.log('Androzon plugin registered');
-}
