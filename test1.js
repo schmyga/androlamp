d7f1d4b789550e5b567ec27afeea9db214477679
schmyga <93492278+schmyga@users.noreply.github.com>
2025-10-06 20:22:47 +0200

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,1 +1,23 @@
-ss
+// Androzon Plugin
+var plugin = {};
+
+plugin.name = 'Androzon';
+plugin.version = '1.0.0';
+plugin.description = 'Search plugin';
+
+plugin.search = function(text, type) {
+    return new Promise((resolve) => {
+        var results = [{
+            title: 'Тест: ' + text,
+            description: 'Androzon работает!',
+            year: '2024',
+            type: type || 'movie'
+        }];
+        resolve(results);
+    });
+};
+
+// Регистрация для Lampa
+if(typeof Manager !== 'undefined') {
+    Manager.add(plugin);
+}
